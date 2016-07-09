72378246e5a1179b8f3e169cd7d7624e0d85e64e
Florent Morselli <florent+git@morselli.fr>
2016-07-09 18:47:14 +0200

    Configuration simplified

--- DependencyInjection/SpomkyLabsLexikJoseExtension.php
+++ DependencyInjection/SpomkyLabsLexikJoseExtension.php
@@ -11,12 +11,14 @@
 
 namespace SpomkyLabs\LexikJoseBundle\DependencyInjection;
 
+use Assert\Assertion;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class SpomkyLabsLexikJoseExtension extends Extension
+class SpomkyLabsLexikJoseExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * @var string
@@ -46,10 +48,10 @@ class SpomkyLabsLexikJoseExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $container->setAlias('lexik_jose_bridge.encoder.jwt_creator', $config['jwt_creator']);
-        $container->setAlias('lexik_jose_bridge.encoder.jwt_loader', $config['jwt_loader']);
+        $container->setAlias('lexik_jose_bridge.encoder.jwt_creator', 'jose.jwt_creator.lexik');
+        $container->setAlias('lexik_jose_bridge.encoder.jwt_loader', 'jose.jwt_loader.lexik');
         $container->setAlias('lexik_jose_bridge.encoder.signature_key', $config['signature_key']);
-        $container->setAlias('lexik_jose_bridge.encoder.keyset', $config['keyset']);
+        $container->setAlias('lexik_jose_bridge.encoder.keyset', 'jose.key_set.lexik');
         $container->setParameter('lexik_jose_bridge.encoder.signature_algorithm', $config['signature_algorithm']);
 
         $container->setParameter('lexik_jose_bridge.encoder.encryption.enabled', $config['encryption']['enabled']);
@@ -61,5 +63,91 @@ class SpomkyLabsLexikJoseExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+        Assertion::keyExists($bundles, 'SpomkyLabsJoseBundle', 'The "Spomky-Labs/JoseBundle" must be enabled.');
+
+        $jose_config = current($container->getExtensionConfig('jose'));
+        $bundle_config = current($container->getExtensionConfig($this->getAlias()));
+        $jose_config['signers'] = array_merge(
+            $jose_config['signers'],
+            [
+                'lexik' => [
+                    'algorithms' => [$bundle_config['signature_algorithm']],
+                ],
+            ]
+        );
+        $jose_config['encrypters'] = array_merge(
+            $jose_config['encrypters'],
+            [
+                'lexik' => [
+                    'key_encryption_algorithms' => [$bundle_config['encryption']['key_encryption_algorithm']],
+                    'content_encryption_algorithms' => [$bundle_config['encryption']['content_encryption_algorithm']],
+                ],
+            ]
+        );
+        $jose_config['jwt_creators'] = array_merge(
+            $jose_config['jwt_creators'],
+            [
+                'lexik' => [
+                    'signer' => 'jose.signer.lexik',
+                    'encrypter' => 'jose.encrypter.lexik',
+                ],
+            ]
+        );
+        $jose_config['verifiers'] = array_merge(
+            $jose_config['verifiers'],
+            [
+                'lexik' => [
+                    'algorithms' => [$bundle_config['signature_algorithm']],
+                ],
+            ]
+        );
+        $jose_config['decrypters'] = array_merge(
+            $jose_config['decrypters'],
+            [
+                'lexik' => [
+                    'key_encryption_algorithms' => [$bundle_config['encryption']['key_encryption_algorithm']],
+                    'content_encryption_algorithms' => [$bundle_config['encryption']['content_encryption_algorithm']],
+                ],
+            ]
+        );
+        $jose_config['checkers'] = array_merge(
+            $jose_config['checkers'],
+            [
+                'lexik' => [
+                    'claims' => ['exp', 'iat', 'nbf'],
+                    'headers' => ['crit'],
+                ],
+            ]
+        );
+        $jose_config['jwt_loaders'] = array_merge(
+            $jose_config['jwt_loaders'],
+            [
+                'lexik' => [
+                    'verifier' => 'jose.verifier.lexik',
+                    'decrypter' => 'jose.decrypter.lexik',
+                    'checker' => 'jose.checker.lexik',
+                ],
+            ]
+        );
+        $jose_config['key_sets'] = array_merge(
+            $jose_config['key_sets'],
+            [
+                'lexik' => [
+                    'keys' => [
+                        'id' => [
+                            $bundle_config['signature_key'],
+                            $bundle_config['encryption']['encryption_key'],
+                        ],
+                    ],
+                ],
+            ]
+        );
+
+        $container->prependExtensionConfig('jose', $jose_config);
     }
 }
--- Resources/doc/Configuration.md
+++ Resources/doc/Configuration.md
@@ -4,59 +4,30 @@ How to configure this bundle?
 # Pre-requisite
 
 This bundle needs services created by the JoseBundle. They are namely:
-* A JWT Creator for token generation.
-* A JWT Loader for token loading. This loader MUST check the following claims and headers: `exp`, `iat`, `nbf` and `crit`.
 * A signature key for token signing. This key can be a private key (if symmetric) or a shared key (if asymmetric).
-* A keyset that contain the signature public key (if symmetric) or signature shared key (if asymmetric).
 
 If you want to encrypt the tokens, you will need the following additional services:
-* An encryption key for token encryption. This key can be a public key (if symmetric) or a shared key (if asymmetric).
-* The keyset MUST also contain the encryption private key (if symmetric) or encryption shared key (if asymmetric).
+* An encryption key for token encryption. This key can be a private key (if symmetric) or a shared key (if asymmetric).
+
+If private keys are provided, public keys are automatically computed.
 
 *Reminder: the Spomky-Labs/JoseBundle creates services according to the ID you set in the configuration*
-*A key with the ID `lexik_private_key` will be available through the `jose.key.lexik_private_key` service.*
+*A key with the ID `lexik_signature_key` will be available through the `jose.key.lexik_signature_key` service.*
 
 Example With Encryption Support
 -------------------------------
 
 ```yml
 jose:
-    easy_jwt_creator: # The JWT Creator
-        lexik:
-            signature_algorithms:
-                - "RS512"
-            key_encryption_algorithms:
-                - "A256GCMKW"
-            content_encryption_algorithms:
-                - "A256GCM"
-    easy_jwt_loader: # The JWT Loader
-        lexik:
-            signature_algorithms:
-                - "RS512"
-            key_encryption_algorithms:
-                - "A256GCMKW"
-            content_encryption_algorithms:
-                - "A256GCM"
-            claim_checkers: # Very important: the following claim checkers MUST be enabled
-                - "exp"
-                - "iat"
-                - "nbf"
-            header_checkers: # Very important: the following header checkers MUST be enabled
-                - "crit"
     keys:
-        lexik_private_key: # We load public and private keys from a file for the signature
+        lexik_signature_key: # We load a private key from a file for the signature
             file:
                 path: "%kernel.root_dir%/keys/private.key"
                 password: "tests"
                 additional_values: # Not mandatory but highly recommended
                     kid: "KEY1"
                     alg: "RS512"
-        lexik_public_key:
-            file:
-                path: "%kernel.root_dir%/keys/public.key"
-                additional_values: # Not mandatory but highly recommended
-                    kid: "KEY1"
-                    alg: "RS512"
+                    use: "sig"
         lexik_encryption_key: # The encryption key is a symmetric key
             values:
                 values:
@@ -65,12 +36,6 @@ jose:
                     use: "enc"
                     alg: "A256GCMKW"
                     k: "qC57l_uxcm7Nm3K-ct4GFjx8tM1U8CZ0NLBvdQstiS8"
-    key_sets:
-        lexik_keyset: # The keyset contains the signature public key and the encryption shared key
-            keys:
-                id:
-                    - "jose.key.lexik_public_key"
-                    - "jose.key.lexik_encryption_key"
 ```
 
 # The Bundle Configuration
@@ -81,11 +46,8 @@ Now that all your services are available, we can configure the bundle.
 
 ```yml
 lexik_jose:
-    jwt_creator: "jose.jwt_creator.lexik"
-    jwt_loader: "jose.jwt_loader.lexik"
-    signature_algorithm: "RS512" # The algorithm MUST be supported by the JWT Creator and the JWT Loader
-    signature_key: "jose.key.lexik_private_key"
-    keyset: "jose.key_set.lexik_keyset"
+    signature_algorithm: "RS512"
+    signature_key: "jose.key.lexik_signature_key"
 ```
 
 ## With Encryption
@@ -96,8 +58,8 @@ lexik_jose:
     encryption:
         enabled: true
         encryption_key: 'jose.key.lexik_encryption_key'
-        key_encryption_algorithm: 'A256GCMKW' # The algorithm MUST be supported by the JWT Creator and the JWT Loader
-        content_encryption_algorithm: 'A256GCM' # The algorithm MUST be supported by the JWT Creator and the JWT Loader
+        key_encryption_algorithm: 'A256GCMKW'
+        content_encryption_algorithm: 'A256GCM'
 ```
 
 # Lexik JWT Authentication Bundle Configuration
