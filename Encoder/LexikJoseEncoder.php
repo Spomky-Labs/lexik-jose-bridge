1759f7d3bb72f243d9c5b9912d1354895485fdb3
Florent Morselli <florent+git@morselli.fr>
2016-07-21 23:32:37 +0200

    Messages updated

--- Encoder/LexikJoseEncoder.php
+++ Encoder/LexikJoseEncoder.php
@@ -114,6 +114,7 @@ class LexikJoseEncoder implements JWTEncoderInterface
 
             return $jwt;
         } catch (\Exception $e) {
+            throw new JWTEncodeFailureException('An error occurred while trying to encode the JWT token: '. $e->getMessage(), $e);
         }
     }
 
@@ -162,6 +163,7 @@ class LexikJoseEncoder implements JWTEncoderInterface
 
             return $jws->getPayload();
         } catch (\Exception $e) {
+            throw new JWTDecodeFailureException('Invalid JWT Token: '.$e->getMessage(), $e);
         }
     }
 
