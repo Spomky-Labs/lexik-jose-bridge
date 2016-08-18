ec9fe7e43313ab80758847ebfb5962bd373ceda9
Florent Morselli <florent+git@morselli.fr>
2016-08-18 22:01:37 +0200

    Claim tests added
    Fixes #8

--- Tests/Bundle/TestBundle/DependencyInjection/SpomkyLabsTestExtension.php
+++ Tests/Bundle/TestBundle/DependencyInjection/SpomkyLabsTestExtension.php
@@ -11,12 +11,16 @@
 
 namespace SpomkyLabs\TestBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class SpomkyLabsTestExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
