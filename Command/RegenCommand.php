f91c86b44f11627063e6a2b33205110644b90b77
Spomky <Spomky@users.noreply.github.com>
2016-09-29 16:40:57 +0200

    Update RegenCommand.php

--- Command/RegenCommand.php
+++ Command/RegenCommand.php
@@ -11,13 +11,9 @@
 
 namespace SpomkyLabs\LexikJoseBundle\Command;
 
-use Jose\Object\JWKSetInterface;
 use Jose\Object\StorableInterface;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
-class RegenCommand extends ContainerAwareCommand
+class RegenCommand extends AbstractCommand
 {
     /**
      * {@inheritdoc}
@@ -38,50 +34,9 @@ EOT
     /**
      * {@inheritdoc}
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function executeCommand(RotatableInterface $jwkset)
     {
-        $services = ['jose.key_set.lexik_jose_signature_keyset'];
-        if ($this->getContainer()->has('jose.key_set.lexik_jose_encryption_keyset')) {
-            $services[] = 'jose.key_set.lexik_jose_encryption_keyset';
-        }
-
-        foreach ($services as $service) {
-            $result = $this->processKeySet($output, $service);
-            if (0 !== $result) {
-                return $result;
-            }
-        }
-        $output->writeln('Done.');
-
-        return 0;
-    }
-
-    /**
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     * @param string                                            $service
-     *
-     * @return int
-     */
-    private function processKeySet(OutputInterface $output, $service)
-    {
-        if (!$this->getContainer()->has($service)) {
-            $output->writeln(sprintf('<error>The service "%s" does not exist.</error>', $service));
-
-            return 1;
-        }
-        $service = $this->getContainer()->get($service);
-        if (!$service instanceof JWKSetInterface) {
-            $output->writeln(sprintf('<error>The service "%s" is not a key set.</error>', $service));
-
-            return 2;
-        }
-
-        if (!$service instanceof StorableInterface) {
-            $output->writeln(sprintf('<error>The service "%s" is not a storable key set.</error>', $service));
-
-            return 3;
-        }
-        $service->regen();
+        $jwkset->regen();
 
         return 0;
     }
