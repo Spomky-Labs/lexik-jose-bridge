489c5e430265e0e6587d2d49f7ea055caaa969b4
Spomky <Spomky@users.noreply.github.com>
2016-09-29 16:45:17 +0200

    Update RotateCommand.php

--- Command/RotateCommand.php
+++ Command/RotateCommand.php
@@ -11,14 +11,12 @@
 
 namespace SpomkyLabs\LexikJoseBundle\Command;
 
-use Jose\Object\JWKSetInterface;
 use Jose\Object\RotatableInterface;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class RotateCommand extends ContainerAwareCommand
+class RotateCommand extends AbstractCommand
 {
     /**
      * {@inheritdoc}
@@ -45,69 +43,19 @@ EOT
     /**
      * {@inheritdoc}
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $services = ['jose.key_set.lexik_jose_signature_keyset'];
-        if ($this->getContainer()->has('jose.key_set.lexik_jose_encryption_keyset')) {
-            $services[] = 'jose.key_set.lexik_jose_encryption_keyset';
-        }
-
-        foreach ($services as $service) {
-            $result = $this->processKeySet($input, $output, $service);
-            if (0 !== $result) {
-                return $result;
-            }
-        }
-        $output->writeln('Done.');
-
-        return 0;
-    }
-
-    /**
-     * @param \Symfony\Component\Console\Input\InputInterface   $input
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     *
-     * @return int
-     */
-    private function processKeySet(InputInterface $input, OutputInterface $output, $service)
-    {
-        if (!$this->getContainer()->has($service)) {
-            $output->writeln(sprintf('<error>The service "%s" does not exist.</error>', $service));
-
-            return 1;
-        }
-        $service = $this->getContainer()->get($service);
-        if (!$service instanceof JWKSetInterface) {
-            $output->writeln(sprintf('<error>The service "%s" is not a key set.</error>', $service));
-
-            return 2;
-        }
-
-        if (!$service instanceof RotatableInterface) {
-            $output->writeln(sprintf('<error>The service "%s" is not a rotatable key set.</error>', $service));
-
-            return 3;
-        }
-        $this->rotateIfNeeded($input, $service);
-
-        return 0;
-    }
-
-    /**
-     * @param \Symfony\Component\Console\Input\InputInterface   $input
-     * @param \Jose\Object\RotatableInterface                   $service
-     */
-    private function rotateIfNeeded(InputInterface $input, RotatableInterface $service)
+    protected function executeCommand(InputInterface $input, OutputInterface $output, RotatableInterface $jwkset)
     {
         $ttl = $input->getArgument('ttl');
         $date = new \DateTime();
         $interval = \DateInterval::createFromDateString($ttl);
         $date = $date->sub($interval);
-        $mtime = $service->getLastModificationTime();
+        $mtime = $jwkset->getLastModificationTime();
         if (null === $mtime) {
             $service->regen();
         } elseif ($mtime <= $date->getTimestamp()) {
-            $service->rotate();
+            $jwkset->rotate();
         }
+
+        return 0;
     }
 }
